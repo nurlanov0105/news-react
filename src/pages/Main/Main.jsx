741231c5443ed2d9d2c5778ed7bc97a344716d9c
Nursultan <nurlanov0105@gmail.com>
2024-02-05 01:16:13 +0600

    add pagination, change api endpoint

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,31 +5,71 @@ import styles from './styles.module.scss';
 import NewsBanner from '../../components/NewsBanner/NewsBanner';
 import NewsList from '../../components/NewsList/NewsList';
 import Skeleton from '../../components/Skeleton/Skeleton';
+import Pagination from '../../components/Pagination/Pagination';
 
 const Main = () => {
    const [news, setNews] = useState([]);
    const [isLoading, setIsLoading] = useState(true);
+   const [currentPage, setCurrentPage] = useState(0);
+   const totalPages = 10;
+   const pageSize = 10;
 
+   const fetchNews = async (currentPage) => {
+      try {
+         setIsLoading(true);
+
+         const response = await getNews(currentPage, pageSize);
+         setNews(response.news);
+
+         setIsLoading(false);
+      } catch (error) {
+         console.log(error);
+      }
+   };
    useEffect(() => {
-      const fetchNews = async () => {
-         try {
-            setIsLoading(true);
+      fetchNews(currentPage);
+   }, [currentPage]);
 
-            const response = await getNews();
-            setNews(response.news);
+   const handleNextPage = () => {
+      if (currentPage < totalPages) {
+         setCurrentPage((current) => current + 1);
+      }
+   };
 
-            setIsLoading(false);
-         } catch (error) {
-            console.log(error);
-         }
-      };
-      fetchNews();
-   }, []);
+   const handlePreviousPage = () => {
+      if (currentPage > 1) {
+         setCurrentPage((current) => current - 1);
+      }
+   };
+
+   const handlePageClick = (pageNumber) => {
+      setCurrentPage(pageNumber);
+   };
 
    return (
       <main className={styles.main}>
-         {!isLoading ? <NewsBanner item={news[17]} /> : <Skeleton count={1} type='banner' />}
+         {news.length > 0 && !isLoading ? (
+            <NewsBanner item={news[news.length - 1]} />
+         ) : (
+            <Skeleton count={1} type='banner' />
+         )}
+         <Pagination
+            totalPages={totalPages}
+            handleNextPage={handleNextPage}
+            handlePreviousPage={handlePreviousPage}
+            handlePageClick={handlePageClick}
+            currentPage={currentPage}
+         />
+
          {!isLoading ? <NewsList news={news} /> : <Skeleton count={10} type='item' />}
+
+         <Pagination
+            totalPages={totalPages}
+            handleNextPage={handleNextPage}
+            handlePreviousPage={handlePreviousPage}
+            handlePageClick={handlePageClick}
+            currentPage={currentPage}
+         />
       </main>
    );
 };
