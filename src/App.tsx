47f9da82720d5e36f21ba69edef8296264ac0091
Nursultan <nurlanov0105@gmail.com>
2024-02-06 22:53:19 +0600

    add switch theme and context

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header';
 import Main from './pages/Main/Main';
+import { useTheme } from './context/ThemeContext';
 
 function App() {
+   const { isDark } = useTheme();
    return (
-      <div className='wrapper'>
+      <div className={`app ${isDark ? 'dark' : 'light'}`}>
          <Header />
          <div className='container'>
             <Main />
